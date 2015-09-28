8816cdc1e167a0e1e19d208165c49b0774f4b93e
howkj1 <jhowk@sai-dev-jim.local>
2015-09-28 14:37:39 -0500

    changed the read-time.rb plugin

--- _plugins/read-time.rb
+++ _plugins/read-time.rb
@@ -1,16 +1,18 @@
-module ReadTimeFilter
+# Outputs the reading time
 
-	def readtime(input)
-	
-		charcount = 4.5
-		wpm = 335
+# Read this in “about 4 minutes”
+# Put into your _plugins dir in your Jekyll site
+# Usage: Read this in about {{ page.content | reading_time }}
 
-		rt = (input.to_f/charcount/wpm).round
-		rt = 1 if rt < 1
-		rt
-		
+module ReadingTimeFilter
+	def reading_time( input )
+		words_per_minute = 180
+
+		words = input.split.size;
+		minutes = ( words / words_per_minute ).floor
+		minutes_label = minutes === 1 ? " minute" : " minutes"
+		minutes > 0 ? "about #{minutes} #{minutes_label}" : "less than 1 minute"
 	end
-
-	Liquid::Template.register_filter self
-
 end
+
+Liquid::Template.register_filter(ReadingTimeFilter)
